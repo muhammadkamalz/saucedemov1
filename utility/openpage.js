c902362bc6a1ee25e4a4dbd8ca9720a6c4510d36
Kamal <kamalzulyansyah@gmail.com>
2023-10-13 11:08:30 +0700

    Fix ukuranny

--- utility/openpage.js
+++ utility/openpage.js
@@ -1,13 +1,18 @@
 const {Builder, Browser} = require('selenium-webdriver')
+const {Options} = require('selenium-webdriver/chrome')
+
 
 async function bukahalaman() {
+    const options = new Options()
+    options.addArguments("start-maximized")
     const driver = await new Builder()
     .forBrowser(Browser.CHROME)
+    .setChromeOptions(options)
     .build()
 
-    const originalWindowSize = await driver.manage().window().getSize();
+    // const originalWindowSize = await driver.manage().window().getSize();
 
-    await driver.manage().window().setSize(originalWindowSize)
+    // await driver.manage().window().setSize(originalWindowSize)
     return driver
 }
 
